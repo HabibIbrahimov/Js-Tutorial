d2ccbb30401c535f99ac117bc1251f9cb244152c
HabibIbrahimov <habibyi@code.edu.az>
2022-01-15 14:39:07 +0400

    refactor: updated

--- Js/Resolving-Rejecting with Value.js
+++ Js/Resolving-Rejecting with Value.js
@@ -1,14 +1,83 @@
 //This is a FAKE Http Request Function
 //It takes 1 second to resolve or reject the promise, depending on the url that is passed in
+// const fakeRequest = (url) => {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			const pages = {
+// 				'/users' : [
+// 					{ id: 1, username: 'Bilbo' },
+// 					{ id: 5, username: 'Esmerelda' }
+// 				],
+// 				'/about' : 'This is the about page!'
+// 			};
+// 			const data = pages[url];
+// 			if (data) {
+// 				resolve({ status: 200, data }); //resolve with a value!
+// 			}
+// 			else {
+// 				reject({ status: 404 }); //reject with a value!
+// 			}
+// 		}, 1000);
+// 	});
+// };
+
+// fakeRequest('/users')
+// 	.then((res) => {
+// 		console.log('Status Code', res.status);
+// 		console.log('Data', res.data);
+// 		console.log('REQUEST WORKED!');
+// 	})
+// 	.catch((res) => {
+// 		console.log(res.status);
+// 		console.log('REQUEST FAILED');
+// 	});
+
+// fakeRequest('/dogs')
+// 	.then((res) => {
+// 		console.log('Status Code', res.status);
+// 		console.log('Data', res.data);
+// 		console.log('REQUEST WORKED!');
+// 	})
+// 	.catch((res) => {
+// 		console.log(res.status);
+// 		console.log('REQUEST FAILED');
+// 	});
+
+
+
+
+
+
+
+//This is a FAKE Http Request Function
+//It takes 1 second to resolve or reject the promise, depending on the url that is passed in
 const fakeRequest = (url) => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			const pages = {
-				'/users' : [
+				'/users'        : [
 					{ id: 1, username: 'Bilbo' },
 					{ id: 5, username: 'Esmerelda' }
 				],
-				'/about' : 'This is the about page!'
+				'/users/1'      : {
+					id        : 1,
+					username  : 'Bilbo',
+					upvotes   : 360,
+					city      : 'Lisbon',
+					topPostId : 454321
+				},
+				'/users/5'      : {
+					id       : 5,
+					username : 'Esmerelda',
+					upvotes  : 571,
+					city     : 'Honolulu'
+				},
+				'/posts/454321' : {
+					id    : 454321,
+					title :
+						'Ladies & Gentlemen, may I introduce my pet pig, Hamlet'
+				},
+				'/about'        : 'This is the about page!'
 			};
 			const data = pages[url];
 			if (data) {
@@ -23,22 +92,39 @@ const fakeRequest = (url) => {
 
 fakeRequest('/users')
 	.then((res) => {
-		console.log('Status Code', res.status);
-		console.log('Data', res.data);
-		console.log('REQUEST WORKED!');
+		console.log(res);
+		const id = res.data[0].id;
+		return fakeRequest(`/users/${id}`);
 	})
-	.catch((res) => {
-		console.log(res.status);
-		console.log('REQUEST FAILED');
+	.then((res) => {
+		console.log(res);
+		const postId = res.data.topPostId;
+		return fakeRequest(`/posts/${postId}`);
+	})
+	.then((res) => {
+		console.log(res);
+	})
+	.catch((err) => {
+		console.log('OH NO!', err);
 	});
 
-fakeRequest('/dogs')
-	.then((res) => {
-		console.log('Status Code', res.status);
-		console.log('Data', res.data);
-		console.log('REQUEST WORKED!');
-	})
-	.catch((res) => {
-		console.log(res.status);
-		console.log('REQUEST FAILED');
-	});
+// ************************************************
+// ATTEMPT 2 (deliberate error to illustrate CATCH)
+// ************************************************
+// fakeRequest('/users')
+// 	.then((res) => {
+// 		console.log(res);
+// 		const id = res.data[0].id;
+// 		return fakeRequest(`/useALSKDJrs/${id}`); //INVALID URL, CATCH WILL RUN!
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 		const postId = res.data.topPostId;
+// 		return fakeRequest(`/posts/${postId}`);
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		console.log('OH NO!', err);
+// 	});
